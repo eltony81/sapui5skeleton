3f80c9de04979905f7731dd33b325e93efc06a45
Antonio Di Fluri <antonio.difluri@espedia.it>
2016-01-19 13:15:12 +0100

    fix grunt dep

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,8 @@ module.exports = function(grunt) {
 				resources: [
 					'<%= dir.bower_components %>/openui5-sap.ui.core/resources',
 					'<%= dir.bower_components %>/openui5-sap.m/resources',
+					'<%= dir.bower_components %>/openui5-sap.ui.layout/resources',
+					'<%= dir.bower_components %>/openui5-sap.ui.commons/resources',
 					'<%= dir.bower_components %>/openui5-themelib_sap_bluecrystal/resources'
 				]
 			},
