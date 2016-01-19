b4fe6bb3a13ebed91ffb2cb7a5a622abe6ed3596
Antonio Di Fluri <antonio.difluri@espedia.it>
2016-01-19 12:30:14 +0100

    bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,53 +2,103 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    nodeunit: {
-      files: ['test/**/*_test.js'],
-    },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib: {
-        src: ['lib/**/*.js']
-      },
-      test: {
-        src: ['test/**/*.js']
-      },
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib: {
-        files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'nodeunit']
-      },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'nodeunit']
-      },
-    },
-    serve: {
-		options: {
-			port: 9876
+	grunt.initConfig({
+
+		dir: {
+			webapp: 'app',
+			dist: 'dist',
+			bower_components: 'bower_components'
+		},
+
+		connect: {
+			options: {
+				port: 9876,
+				hostname: '*'
+			},
+			src: {},
+			dist: {}
+		},
+
+		openui5_connect: {
+			options: {
+				resources: [
+					'<%= dir.bower_components %>/openui5-sap.ui.core/resources',
+					'<%= dir.bower_components %>/openui5-sap.m/resources',
+					'<%= dir.bower_components %>/openui5-themelib_sap_bluecrystal/resources'
+				]
+			},
+			src: {
+				options: {
+					appresources: '<%= dir.webapp %>'
+				}
+			},
+			dist: {
+				options: {
+					appresources: '<%= dir.dist %>'
+				}
+			}
+		},
+
+		openui5_preload: {
+			component: {
+				options: {
+					resources: {
+						cwd: '<%= dir.webapp %>',
+						prefix: 'sapui5skeleton'
+					},
+					dest: '<%= dir.dist %>'
+				},
+				components: true
+			}
+		},
+
+		clean: {
+			dist: '<%= dir.dist %>/'
+		},
+
+		copy: {
+			dist: {
+				files: [ {
+					expand: true,
+					cwd: '<%= dir.webapp %>',
+					src: [
+						'**',
+						'!test/**'
+					],
+					dest: '<%= dir.dist %>'
+				} ]
+			}
+		},
+
+		eslint: {
+			webapp: ['<%= dir.webapp %>']
 		}
-	}
-  });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-serve');
-  
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
+	});
 
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-openui5');
+	grunt.loadNpmTasks('grunt-eslint');
+
+	// Server task
+	grunt.registerTask('serve', function(target) {
+		grunt.task.run('openui5_connect:' + (target || 'src') + ':keepalive');
+	});
+
+	// Linting task
+	grunt.registerTask('lint', ['eslint']);
+
+	// Build task
+	grunt.registerTask('build', ['openui5_preload', 'copy']);
+
+	// Default task
+	grunt.registerTask('default', [
+		'lint',
+		'clean',
+		'build',
+		'serve:dist'
+	]);
 };
