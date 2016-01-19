b01c44652b56b1047f32028b935388926becfc43
Antonio Di Fluri <antonio.difluri@espedia.it>
2016-01-19 09:17:37 +0100

    new commit code

---
+++ controller/App.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define( ["sap/ui/core/mvc/Controller"], function (Controller) {
+	"use strict";
+
+	return Controller.extend("tmpproject.controller.App", {
+		/*onInit : function () {
+			var sUrl = "#" + this.getOwnerComponent().getRouter().getURL("page2");
+			this.byId("link").setHref(sUrl);
+            this.getView().setModel(new sap.ui.model.json.JSONModel("model/coffee.json"));
+		},
+
+		onToPage2 : function () {
+			this.getOwnerComponent().getRouter().navTo("page2");
+		}*/
+        
+       
+	});
+
+}, /* bExport= */ true);
